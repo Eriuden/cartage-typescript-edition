d8339e75f01056150d40370bae2c7089fdb93da0
unknown <antoinecarrette@hotmail.fr>
2026-02-15 09:22:12 +0100

    build: searcher component 15/02/2026

---
+++ Client/src/components/Searcher.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getAllCards } from "../redux/actions/card.action"
+import Card from "./Card"
+import { isEmpty } from "../Utils"
+
+export const Searcher = () => {
+
+    type appDispatch = () => any
+    const [search, setSearch] = useState("")
+    const cards = useSelector((state: any) => state.allCardsReducer)
+    const useAppDispatch = () => useDispatch<appDispatch>()
+    const dispatch = useAppDispatch()
+
+    useEffect(()=> {
+        getAllCards(cards, dispatch)
+    }, [search])
+
+    const handleSearchChange = (e: any) => {
+        setSearch(e.target.value)
+
+        const result = cards.filter((card: any)=> card.name.toLowerCase()
+        .includes(search.toLowerCase()) 
+        || card.nation.toLowerCase().includes(search.toLowerCase())
+        || card.grade.toLowerCase().includes(search.toLowerCase()))
+
+        setSearch(result)
+    }
+  return (
+    <div className='flex justify-center'>
+      <form action='' className='border-2 rounded-sm my-4 max-w-[100%] border-black'>
+        <input className='text-center' type="text" placeholder="card/nation/grade"
+        onChange={handleSearchChange} value={search}/>
+      </form>
+
+      <div>
+        { !isEmpty(cards[0]) && search !="" ? (
+          cards.map((card:any) => {
+            if (card.name.includes(search)){
+              <Card articleProps={card} key={card._id}/>
+            }
+          })
+        ): ""}
+      </div>
+    </div>
+  )
+}
