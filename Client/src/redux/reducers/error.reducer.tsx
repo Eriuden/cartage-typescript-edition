13153aff8ce639b4bbda12bb10919f4e81afdaa6
unknown <antoinecarrette@hotmail.fr>
2026-02-15 09:07:42 +0100

    build: client errors reducer 15/02/2026

--- Client/src/redux/reducers/error.reducer.tsx
+++ Client/src/redux/reducers/error.reducer.tsx
@@ -1,11 +1,11 @@
-import { GET_USER_ERRORS } from "../actions/user.actions";
+import { GET_USER_ERROR } from "../actions/user.actions";
 import { GET_CARD_ERROR } from "../actions/card.action";
 
 const initialState = { userError : []}
 
 export default function errorReducer(state = initialState, action:any) {
     switch (action.type) {
-        case GET_USER_ERRORS:
+        case GET_USER_ERROR:
             return {
                 userError: action.payload
             }
