25c9e869c01c15eaa4b9b06432ba6111a8407fcb
unknown <antoinecarrette@hotmail.fr>
2026-02-17 14:31:45 +0100

    build: util 15/02/2026

---
+++ Client/src/pages/singleCardPage.tsx
@@ -0,0 +1,30 @@
+import { getCard } from '../redux/actions/card.action'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+
+
+//Ce sera la page pour chaque carte
+//Il faudra relier au get card (et non cards) de l'api
+//Il suffisait de directement mettre le result
+
+export default function singleCardPage() {
+
+  type appDispatch = () => any
+  const card = useSelector((state: any) => state.cardReducer)
+  const useAppDispatch = () => useDispatch<appDispatch>()
+  const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        getCard(card, dispatch)
+      })
+  return (
+    <div>
+        <h2>{card.name}</h2>
+        <img src={card.picturte} alt="image us"/>
+        <h2>{card.power}</h2>
+        <h2>{card.nation}</h2>
+        <h2>{card.effect}</h2>
+        <h2>{card.price}</h2>
+    </div>
+  )
+}
